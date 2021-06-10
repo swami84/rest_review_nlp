191b8716efbf1deb2f3096b84a342bf9e1ed1966
swami84 <swami.me@gmail.com>
2021-06-10 16:49:05 -0400

    Commit to move stuff out of the laptop

--- src/utils/etl.py
+++ src/utils/etl.py
@@ -1,9 +1,6 @@
-import pandas as pd
-import requests
+
 import json
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
+
 import os
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.keys import Keys
@@ -15,36 +12,38 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from datetime import datetime
 import time
-import re
-import logging
-import traceback
+
 
 class ReviewScraper():
     def __init__(self):
         self.driver = self.__get_driver()
 
-    def __get_driver(self,debug=False):
+    def __get_driver(self,debug=True):
         options = Options()
 
         if not debug:
             options.add_argument("--headless")
+            options.add_argument('--no-sandbox')
         else:
             options.add_argument("--window-size=1366,768")
+            options.add_argument('--no-sandbox')
 
         options.add_argument("--disable-notifications")
         options.add_argument("--lang=en-US")
         driv_path = '../config/webdrivers/chromedriver.exe'
         input_driver = webdriver.Chrome(options=options, executable_path=driv_path)
+        print('Driver Got')
         return input_driver
     def __sort_review(self):
         wait = WebDriverWait(self.driver, 10)
 
         menu_bt = wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@data-value=\'Sort\']')))
         menu_bt.click()
-        time.sleep(3)
+        time.sleep(1)
         recent_rating_bt = self.driver.find_elements_by_xpath('//li[@role=\'menuitemradio\']')[1]
         recent_rating_bt.click()
-        time.sleep(2)
+        time.sleep(1)
+        print('Reviews Sorted')
 
     def __goto_review(self):
         links = self.driver.find_elements_by_xpath('//button[@jsaction=\'pane.reviewChart.moreReviews\']')
@@ -53,17 +52,27 @@ class ReviewScraper():
             # print("Element is visible? " + str(l.is_displayed()))
             l.click()
             time.sleep(2)
+
     def __scroll(self):
+        reviews_divs = self.driver.find_elements_by_class_name('section-layout')
+        reviews_divs[-1].click()
         scrollable_div = self.driver.find_element_by_css_selector(
-            'div.section-layout.section-scrollbox.scrollable-y.scrollable-show')
-        self.driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', scrollable_div)
-        time.sleep(0.5)
+            'div.section-layout.section-scrollbox.mapsConsumerUiCommonScrollable__scrollable-y.mapsConsumerUiCommonScrollable__scrollable-show')
+        #
+        # self.driver.execute_script('arguments[0].scrollTop = arguments[0].scrollHeight', scrollable_div)
+        # print(self.driver.execute_script("return document.body.scrollTop"))
+        # print(self.driver.execute_script("return document.documentElement.scrollHeight"))
+        height = self.driver.execute_script("return document.documentElement.scrollHeight",scrollable_div)
+        print(height)
+        self.driver.execute_script("window.scrollTo(0, " + str(height) + ");")
+        time.sleep(2)
 
     def __expand_reviews(self):
-        links = self.driver.find_elements_by_xpath('//button[@class=\'section-expand-review blue-link\']')
+        links = self.driver.find_elements_by_xpath('//button[@class=\'section-expand-review mapsConsumerUiCommonButton.blue-link\']')
         for l in links:
             l.click()
-            time.sleep(1)
+            time.sleep(0.5)
+            print('Review Expanded')
 
     def url_setup(self,url):
         self.driver.get(url)
@@ -81,7 +90,7 @@ class ReviewScraper():
 
         resp = BeautifulSoup(self.driver.page_source, 'html.parser')
 
-        rblock = resp.find_all('div', class_='section-review-content')
+        rblock = resp.find_all('div', class_='section-review-text')
         parsed_reviews = []
         for index, review in enumerate(rblock):
             if index >= offset:
